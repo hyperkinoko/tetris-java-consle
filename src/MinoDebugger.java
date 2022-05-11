551352c76dcef2f230b2abb63e2d77bdddbc6600
hyperkinoko <superkinoko@icloud.com>
2022-05-11 15:38:28 +0900

    answerd

--- src/MinoDebugger.java
+++ src/MinoDebugger.java
@@ -1,8 +1,12 @@
 public class MinoDebugger {
     public static void main(String[] args) {
         Mino mino = new Mino();
-        for(int i = 0; i < 4; i++) {
-            mino.debugPrint(i, MinoType.MINO_T);
+        for(MinoType minoType : MinoType.values()) {
+            System.out.println(minoType);
+            for(int i = 0; i < 4; i++) {
+                mino.debugPrint(i, minoType);
+                System.out.println();
+            }
         }
     }
 }
