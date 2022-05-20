4b2a4b616fb736e2e3ce109f05144ea09240172b
hyperkinoko <superkinoko@icloud.com>
2022-05-20 21:38:38 +0900

    step17 end

--- src/GameThread.java
+++ src/GameThread.java
@@ -7,7 +7,7 @@ public class GameThread extends Thread {
     private Mino mino;
     private Mino nextMino;
     private String playerName;
-    private int score = 0;
+    private int score = 567;
     private int waitTime = 1000;
     
     public GameThread(GameArea ga, String playerName) {
@@ -72,20 +72,44 @@ public class GameThread extends Thread {
     private void gameOver() {
         System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
         System.out.println();
-        System.out.println("　　　　　　　Game Over!!");
+        System.out.println("　　　　　Game Over!!");
         System.out.println();
-        System.out.println(String.format("あなたのスコア: %3d", score));
-        System.out.println();
-        System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
-        System.out.println();
-        System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
-        System.out.println();
-        System.out.println("　　　　　　　ランキング");
-        System.out.println();
-        for(Ranking ranking: rankingList) {
-            
-        }
         System.out.println(String.format("　　　あなたのスコア: %3d", score));
+        System.out.println();
+        System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+        System.out.println();
+        System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+        System.out.println();
+        System.out.println("　　　　　ランキング");
+        System.out.println();
+
+        Ranking[] rankingList = new Ranking[10];
+        rankingList[0] = new Ranking(123, 789, "キノコ");
+        rankingList[1] = new Ranking(2, 789, "nktr");
+        rankingList[2] = new Ranking(1, 789, "村田");
+        rankingList[3] = new Ranking(34, 567, "Kinoko");
+        rankingList[4] = new Ranking(21, 567, "tomotomo");
+        rankingList[5] = new Ranking(13, 567, "YOSHIKI");
+        rankingList[6] = new Ranking(32, 456, "yosuke");
+        rankingList[7] = new Ranking(12, 345, "DAIDAI");
+        rankingList[8] = new Ranking(3, 234, "moriya");
+        rankingList[9] = new Ranking(85, 123, "キノコ");
+
+        int rank = 0;
+        while(rank < 10) {
+            if(rankingList[rank].getScore() == score && rankingList[rank].getName().equals(playerName)) {
+                break;
+            }
+            rank++;
+        }
+        System.out.println("　　あなたは" + (rank < 10?rank+1+"位":"ランク外") + "でした！！");
+        System.out.println();
+
+        for(int i = 0; i < rankingList.length; i++) {
+            Ranking ranking = rankingList[i];
+            String yourRankIsHere = (ranking.getScore() == score && ranking.getName().equals(playerName)) ? " <-- YOU!!" : "";
+            System.out.println(String.format("　　 %2d位: %s %s", i+1, ranking, yourRankIsHere));
+        }
         System.out.println();
         System.out.println("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
     }
--- src/Ranking.java
+++ src/Ranking.java
@@ -1,6 +1,28 @@
 public class Ranking {
+    private int id;
     private int score;
     private String name;
-    
-    public()
+
+    public Ranking(int id, int score, String name) {
+        this.id = id;
+        this.score = score;
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%03d -- %-10s", score, name);
+    }
 }
